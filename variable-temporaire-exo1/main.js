922f82e58c59a19c2095c1013a6ee12962ceed4b
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-21 01:22:16 +0200

    Add files via upload

---
+++ variable-temporaire-exo1/main.js
@@ -0,0 +1,71 @@
+// EXO 1
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let temp;
+// temp = a;
+// a = b;
+// b = c;
+// c = temp;
+
+// console.log(a,b,c)
+// EXO 2
+// let a = 1;                                              // 4
+// let b = 2;                                              // 3
+// let c = 3;                                              // 2
+// let d = 4;                                              // 1
+
+// let temp = a;
+// a = d;
+// d = temp;
+// temp = b;
+// b = c;
+// c = temp
+
+// console.log(a,b,c,d)
+
+//EXO 3
+// let tableau = [1,2,3,4];                // 4  3  2  1
+// let temp = tableau[3];
+
+// tableau[3] = tableau[0];
+// tableau[0] = temp;
+// temp = tableau[2]
+// tableau[2] = tableau[1]
+// tableau[1] = temp
+
+// console.log(tableau)
+
+let a = 1;                               // 4
+let b = 2;                               // 5
+let c = 3;                               // 1
+let d = 4;                               // 2
+let e = 5;                               // 6
+let f = 6;                               // 3
+
+let temp = a;
+a    = d;
+d    = temp;
+temp = b;
+b    = e;
+e    = temp;
+temp = c;
+c    = d;
+d    = temp;
+temp = d;
+d    = e;
+e    = temp;
+temp = e;
+e    = f;
+f    = temp;
+
+
+
+
+
+
+
+
+console.log(a,b,c,d,e,f);
+
